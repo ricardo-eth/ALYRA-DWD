ee20bf261d7378941506503f6b3fd26a481747cd
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-02-25 14:26:55 +0100

    Add vote.js

---
+++ EXO-JS/exo-expressions-conditions/vote.js
@@ -0,0 +1,10 @@
+let age = 20
+let canVote = false
+
+let msg = age >= 18 ? canVote = true : canVote = false
+
+let youCanVote = canVote === true ? 'You can vote' : 'You can not vote'
+
+console.log(youCanVote)
+
+
