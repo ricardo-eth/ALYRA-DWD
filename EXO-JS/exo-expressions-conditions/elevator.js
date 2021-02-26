01319136f79c429b7da0397cc05d819d15056d81
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-02-26 10:13:29 +0100

    elevator.js (in progress)

--- EXO-JS/exo-expressions-conditions/elevator.js
+++ EXO-JS/exo-expressions-conditions/elevator.js
@@ -1,13 +1,23 @@
-let currentFloor = '1'
-let targetFloor = '1'
+let currentFloor = '5'
+let targetFloor = '-11'
 
 let verification = Number(targetFloor) != Number(currentFloor)
+let horsEtage = currentFloor >= -2 && currentFloor <= 7
 
-switch (verification) {
+let formule = verification + horsEtage
+
+console.log(horsEtage)
+
+switch (formule) {
   
-  case true:
+  case true :
     console.log(
       `Vous etes au niveau ${currentFloor} et vous voulez aller au niveau ${targetFloor}`
+    )
+    break
+  case false:
+    console.log(
+      `Le niveau demandé existe pas`
     )
     break
       default:
