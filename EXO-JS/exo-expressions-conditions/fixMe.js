d593b62962adc97c63d9d5ebc9f80cbbd11f1284
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-02-26 11:18:43 +0100

    Fix

--- EXO-JS/exo-expressions-conditions/fixMe.js
+++ EXO-JS/exo-expressions-conditions/fixMe.js
@@ -1,17 +1,21 @@
-let list = ['Alice', 'Bob', 'Charlie', 'Craig', 'Eva']
+let list = ['Alice', 'Bob', 'Charlie', 'Craig', 'Eve']
 
-for (let i = 0; i <= list.length; ++i) {
-list = list[i].toUpperCase()
+for (let i = 0; i < list.length; ++i) {
+list[i] = list[i].toUpperCase()
 
- switch (list) {
+ switch (list[i]) {
     case 'ALICE':
       console.log('Alice want to exchange with Bob')
+      break
     case 'BOB':
       console.log('Bob want to exchange with Alice')
+      break
     case 'CHARLIE':
       console.log('Charlie is a generic third participant')
+      break
     case 'CRAIG':
       console.log('Craig is a password cracker')
+      break
     case 'EVE':
       console.log('Eve is an eavesdropper')
   }
